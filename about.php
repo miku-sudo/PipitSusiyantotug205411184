4cbc14ac660206c358d9cf20c78a8cd68d01b7eb
PIPIT SUSIYANTO <59631465+miku-sudo@users.noreply.github.com>
2021-08-07 18:53:51 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PipitSusiyantotug205411184</title>
+</head>
+    <body>
+        <table border="1" width="50%">
+            <tr> <th>Nama</th><th>Pipit Susiyanto</th> 
+			<tr> <th>Nim</th><th>205411184</th>
+			<tr> <th>Jurusan</th><th>Teknik Informatika</th>
+			<tr> <th>Tempat Tinggal</th><th>Purworejo, Jawa Tengah</th>
+        </table> 
+			<p>Untuk lebih lengkapnya, silahkan buka:
+            <a href="profil.php">Lainnya</a>
+			</p>		
+    </body>
+</html>
---
+++ profil.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PipitSusiyantotug205411184</title>
+</head>
+    <body>
+        <table border="2" width="50%">
+          <tr>  <th>Android adalah sebuah sistem operasi yang sudah menjadi salah satu kebutuhan pokok. 
+			<br>Hampir semua orang membutuhkan alat yang satu ini untuk berkomunikasi,
+			<br>mencari informasi dan perkembangan berita terbaru,melakukan pekerjaan, dan berbagai aktivitas lainnya. 
+			<br>Bahkan, saat ini hampir semua orang menggantungkan aktivitas mereka pada Android yang mereka miliki.
+        
+		</table> 
+			<p>Untuk lebih lengkapnya, silahkan buka:
+            <a href="main.php">About us</a>
+			</p>		
+    </body>
+</html>
